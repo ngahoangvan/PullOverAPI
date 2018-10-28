eddcd75cf449e4cf94929afb22323b7ea973bd9f
Van Nga <ngahv2222@gmail.com>
2018-10-28 23:30:45 +0700

    Update 28/10/2018: Account API

---
+++ source/account/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import signals
+from tastypie.models import create_api_key
+
+# Create your models here.
+signals.post_save.connect(create_api_key, sender=User)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    other_name = models.CharField(max_length=255, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=255, blank=True)
+    photo_url = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Relationship(models.Model):
+    STATUS_IN_RELATIONSHIP = (
+        (0, 'sending'),
+        (1, 'accepted'),
+        (2, 'unfriend'),
+        (3, 'blocked'),
+    )
+    user_one = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='user_one')
+    user_two = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='user_two')
+    status = models.IntegerField(choices=STATUS_IN_RELATIONSHIP, default=0)
+    is_friends = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s and %s: %s' % (self.user_one.get_full_name(),
+                                  self.user_two.get_full_name(),
+                                  self.get_status_display())
